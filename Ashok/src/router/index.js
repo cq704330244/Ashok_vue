d67adbdf02b73da8c7c3f5dd2a35c108fc0b3ecd
程奇 <wb-cq656493@antfin.com>
2020-05-03 11:01:59 +0800

    feat: add 404 router

--- Ashok/src/router/index.js
+++ Ashok/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../view/login/Login'
 import Home from '../view/home/index.vue'
+import Notfound from '../view/notFound/index.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +16,11 @@ export default new Router({
       path: '/home',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '*',
+      name: '404notFound',
+      component: Notfound
     }
   ]
 })
