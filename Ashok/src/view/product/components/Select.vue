ad35ba1d96e030829d9c832d2e191e792470ad8f
Vincent <vovi.chen@gmail.com>
2020-05-04 19:53:06 +0800

    feat :新增table功能页 封装select 封装分页器 scorll

---
+++ Ashok/src/view/product/components/Select.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <label :for="this.selectvalue">{{this.name}}</label>
+    <el-select
+      :id="this.selectvalue"
+      v-model="sel"
+      placeholder="选择"
+      clearable
+      filterable
+      style="width:120px"
+      @change="handleSelect()"
+    >
+      <el-option v-for="item in item" :key="item.lable" :label="item.lable" :value="item.lable"></el-option>
+    </el-select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Select',
+  props: {
+    item: {
+      type: Array
+    },
+    name: {
+      type: String
+    },
+    selectvalue: {
+      type: String
+    },
+    id: {
+      type: Number
+    }
+  },
+  data() {
+    return {
+      sel: ''
+    }
+  },
+  methods: {
+    handleSelect() {
+      this.$emit('select', this.sel, this.id)
+    }
+  }
+}
+</script>
--- Ashok/src/view/product/index.vue
+++ Ashok/src/view/product/index.vue
@@ -1,13 +1,230 @@
 <template>
-  <div>
-    product
+  <div class="app-contianer">
+    <div class="contianer">
+      <div class="as-input">
+        <label for="input">请搜索英雄：</label>
+        <el-input
+          id="input"
+          v-model="listquery.title"
+          clearable
+          placeholder="请输入英雄名字"
+          style="width:200px"
+          @keyup.native.enter="handleFilter"
+        ></el-input>
+      </div>
+      <Select
+        class="as-select"
+        v-for="(item,index) in selects"
+        :key="index"
+        :name="item.name"
+        :selectvalue="item.value"
+        :item="item.arr"
+        :id="item.id"
+        @select="showModel(arguments)"
+      />
+      <el-button class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">查询</el-button>
+    </div>
+    <el-table
+      :data="list"
+      :key="tableKey"
+      v-loading="showLoading"
+      stripe
+      border
+      fit
+      highlight-current-row
+      class="as-table"
+    >
+      <el-table-column type="selection" width="55"></el-table-column>
+      <el-table-column label="ID" prop="id" sortable="custom" align="center" width="80">
+        <template slot-scope="{row}">
+          <span>{{row.id}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="属性" width="120px" align="center">
+        <template slot-scope="{row}">
+          <span>{{row.propsData}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="定位" width="90px" align="center">
+        <template slot-scope="{row}">
+          <span>{{row.prosition}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="武器" width="140px" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.arms }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="名字" width="244px" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.uname }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="角色" width="90px" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.dayA }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="推图" width="90px" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.dayB }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="BOSS" width="90px" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.dayC }}</span>
+        </template>
+      </el-table-column>
+    </el-table>
+    <Pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="listquery.page"
+      :limit.sync="listquery.limit"
+      @pagination="getList"
+      class="pagination"
+    />
   </div>
 </template>
 
 <script>
+import Select from './components/Select.vue'
+import Pagination from '../../components/Pagination'
 export default {
-  name: 'product'
+  name: 'product',
+  components: {
+    Select,
+    Pagination
+  },
+  data() {
+    return {
+      list: '',
+      showLoading: true,
+      tableKey: 0,
+      total: 1,
+      model: [undefined, undefined, undefined],
+      listquery: {
+        title: '',
+        selectProp: '',
+        prosition: '',
+        arms: '',
+        page: 1,
+        limit: 20
+      },
+      selects: [
+        {
+          id: 0,
+          name: '属性：',
+          value: 'selectProp',
+          arr: [
+            { value: '选项一', lable: '机械' },
+            { value: '选项二', lable: '生物' },
+            { value: '选项三', lable: '幽能' },
+            { value: '选项四', lable: '防疫' },
+            { value: '选项五', lable: '侵蚀' }
+          ]
+        },
+        {
+          id: 1,
+          name: '定位：',
+          value: 'prosition',
+          arr: [
+            { value: '选项一', lable: '火力' },
+            { value: '选项一', lable: '强袭' },
+            { value: '选项一', lable: '辅助' },
+            { value: '选项一', lable: '医疗' },
+            { value: '选项一', lable: '防御' }
+          ]
+        },
+        {
+          id: 2,
+          name: '武器：',
+          value: 'arms',
+          arr: [
+            { value: '选项一', lable: '自动步枪' },
+            { value: '选项一', lable: '手枪' },
+            { value: '选项一', lable: '霰弹枪' },
+            { value: '选项一', lable: '榴弹枪' },
+            { value: '选项一', lable: '狙击枪' }
+          ]
+        }
+      ]
+    }
+  },
+  methods: {
+    // 子传父 封装select
+    showModel(res) {
+      this.model[res[1]] = res[0]
+      let _that = this.listquery
+      _that.selectProp = this.model[0]
+      _that.prosition = this.model[1]
+      _that.arms = this.model[2]
+    },
+    // table list
+    getList() {
+      this.list = [
+        {
+          id: 1,
+          propsData: '机械',
+          prosition: '火力',
+          arms: '手枪',
+          uname: '战术装备 - 时无暇 ',
+          dayA: 'S',
+          dayB: 'A',
+          dayC: 'S'
+        },
+        {
+          id: 2,
+          propsData: '生物',
+          prosition: '辅助',
+          arms: '自动步枪',
+          uname: '私服 - 艾琳',
+          dayA: 'B',
+          dayB: 'B',
+          dayC: 'B'
+        }
+      ]
+      this.showLoading = false
+    },
+    // filter事件流
+    handleFilter() {
+      this.listquery.page = 1
+      this.getList()
+      this.$message({
+        message: '恭喜您，查询成功',
+        type: 'success'
+      })
+      console.log(this.listquery)
+    }
+  },
+  created() {
+    this.getList()
+  }
 }
 </script>
 
-<style></style>
+<style>
+.contianer {
+  position: relative;
+  top: 20px;
+  left: 270px;
+}
+.as-input {
+  float: left;
+}
+.as-select {
+  float: left;
+  width: 160px;
+  padding: 0 0px 0 20px;
+}
+.filter-item {
+  margin: 0 0 0 20px;
+}
+.as-table {
+  width: 1000px;
+  margin: 60px auto;
+}
+.pagination {
+  text-align: center;
+}
+</style>
