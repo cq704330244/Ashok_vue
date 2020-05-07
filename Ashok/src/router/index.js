102d5057dffd8e39e86683d237160950785966fb
程奇 <wb-cq656493@antfin.com>
2020-05-08 00:51:42 +0800

    fix: reapir router error

--- Ashok/src/router/index.js
+++ Ashok/src/router/index.js
@@ -49,3 +49,7 @@ export default new Router({
     }
   ]
 })
+const routerPush = Router.prototype.push
+Router.prototype.push = function push(location) {
+  return routerPush.call(this, location).catch(error => error)
+}
