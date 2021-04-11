bc9e70aa36f7eb60d58d1f9cb0ea90f9973680da
LAPTOP-N7IVA5JC\chengqi <704330244@qq.com>
2021-04-11 12:34:22 +0800

    fix: style

--- Ashok/src/view/tree/personItem.vue
+++ Ashok/src/view/tree/personItem.vue
@@ -12,21 +12,23 @@
       :value="item"
       v-model="checkedPersonData"
     />
-    <img :src="item.avart" alt="" />
-    <div class="item-space">
-      <div class="first">
-        <div>{{ item.name }}</div>
-        <div class="tag">{{ item.department }}</div>
+    <div class="contens">
+      <img :src="item.avart" alt="" />
+      <div class="item-space">
+        <div class="first">
+          <div>{{ item.name }}</div>
+          <div class="tag">{{ item.department }}</div>
+        </div>
+        <div class="last">{{ item.department }}</div>
       </div>
-      <div class="last">{{ item.department }}</div>
+      <van-icon
+        v-if="see"
+        name="close"
+        @click="$emit('deletePerson')"
+        color="#1890ff"
+        size="16"
+      />
     </div>
-    <van-icon
-      v-if="see"
-      name="close"
-      @click="$emit('deletePerson')"
-      color="#1890ff"
-      size="16"
-    />
   </div>
 </template>
 
@@ -54,7 +56,15 @@ export default {
   display: flex;
   justify-content: flex-start;
   align-items: center;
-  padding: 10px 20px;
+  padding: 0px 20px;
+  .contens {
+    flex: 1 1 auto;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    border-bottom: 1px solid #eee;
+    padding: 10px 0;
+  }
   img {
     display: block;
     width: 36px;
